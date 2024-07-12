d24d4874253ae385f979e9be3fb24dfcf9e08b84
Oli Bridge <ob344@cam.ac.uk>
2024-07-12 11:32:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let mourseCursor = document.querySelector('.cursor');
 let nam = document.querySelector('#name');
+let links = document.querySelectorAll('#links a');
+let content_links = document.querySelectorAll('.content a');
 
 window.addEventListener('mousemove', cursor);
 
@@ -15,4 +17,26 @@ nam.addEventListener('mouseover', () => {
 
 nam.addEventListener('mouseleave', () => {
     mourseCursor.classList.remove('cursor-big');
+});
+
+// Make cursor smaller when hover on the links
+links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        mourseCursor.classList.add('cursor-med');
+    });
+
+    link.addEventListener('mouseleave', () => {
+        mourseCursor.classList.remove('cursor-med');
+    });
+});
+
+// Make cursor smaller when hover on the content links
+content_links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        mourseCursor.classList.add('cursor-med');
+    });
+
+    link.addEventListener('mouseleave', () => {
+        mourseCursor.classList.remove('cursor-med');
+    });
 });
